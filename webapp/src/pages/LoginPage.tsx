a408c20271996ca01cd8dc6134fc79370b49c53c
SoniaFernandezCoto <79599350+SoniaFernandezCoto@users.noreply.github.com>
2022-03-03 18:04:40 +0100

    Ampliación de funcionalidad + diseño

--- webapp/src/pages/LoginPage.tsx
+++ webapp/src/pages/LoginPage.tsx
@@ -1,35 +1,46 @@
 import React, {Fragment, FC, useEffect, useState} from "react";
 import TextField from '@mui/material/TextField';
-import "bootswatch/dist/lumen/bootstrap.min.css";
+import "bootswatch/dist/superhero/bootstrap.min.css"
 import Button from '@mui/material/Button';
 import {Container, Card , CardContent, Grid} from "@mui/material";
 import Link from '@mui/material/Link';
+import logo from '../img/logo-dede.svg'
+
+const checkParams = (text: string) => {
+    return text == "" || text == null;
+}
 
 interface LoginPageProps {
     translate: (key: string) => string
 }
 
+
 const LoginPage: FC<LoginPageProps> = ({ translate }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [loginPod, setLoginPod] = useState(false);
+    const [pulsed, setPulsed] = useState(false);
     return(
     <div>
         <Container component="main" maxWidth="sm">
         <Card className={"main"} elevation={10} style={{display: "grid"}}>
         <CardContent style={{ display: "grid", margin: "auto", textAlign: "center" }}>
-
+            <div>
+            <img  width={150} height = {150} src={logo} />
+            </div>
                 <h1>{translate('login.h1')}</h1>
                 <h3>{translate('login.h2')}</h3>
                     <Fragment>
                         <form>
                             <TextField
+                        id = "textUser"
                         required
                         size="small"
                         name="username"
                         label= {translate ('login.solidUser')} 
                         variant="outlined"
                         value={username}
+                        error = {(checkParams(username) && pulsed)}
+                        helperText= {translate('login.input')}
                         onChange={e => setUsername(e.target.value)}
                         sx={{ my: 2 }}
                         /> 
@@ -41,13 +52,14 @@ const LoginPage: FC<LoginPageProps> = ({ translate }) => {
                         size="small"
                         label= {translate ('login.solidPass')} 
                         variant="outlined"
-                        value={password}
+                        value={password} error = {checkParams(password) && pulsed}
                         onChange={e => setPassword(e.target.value)}
+                        helperText= {translate('login.input')}
                         sx={{ my: 2 }}
                         /> 
                         </form>
                     </Fragment>
-                    <Button variant="contained" type="submit" sx={{ my: 2 }}>{translate('login.solid')}</Button>
+                    <Button onClick={() => setPulsed(true)} variant="contained" type="submit" sx={{ my: 2 }}>{translate('login.solid')}</Button>
             <Link href="https://inrupt.net/register">{translate('login.signup')}</Link>
             </CardContent>
             </Card>
