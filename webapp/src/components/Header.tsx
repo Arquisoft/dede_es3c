149190e18893aa1d75e8a12f3c2a2c170e6ed6f2
SoniaFernandezCoto <79599350+SoniaFernandezCoto@users.noreply.github.com>
2022-03-10 23:13:22 +0100

    Unido con la navBar de alejandro + arreglados cambios surante sesion

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,8 +1,19 @@
 import React, { useCallback, useState, useEffect, useRef, useContext, FC, Fragment } from 'react';
-import {Navbar, Form, Nav, Button} from "react-bootstrap";
+import {Navbar, Form, Nav, Button, NavDropdown, DropdownButton, Dropdown, Container} from "react-bootstrap";
 import "bootswatch/dist/superhero/bootstrap.min.css"
 import { LangContext } from '../lang';
 import { UserContext } from '../User';
+import logo from '../img/logo-dede.svg'
+import homeIcon from '../img/home-icon.svg'
+import catalogIcon from '../img/catalog-icon.svg'
+import englishIcon from '../img/english-icon.svg'
+import loginIcon from '../img/login-icon.svg'
+import logoutIcon from '../img/logout-icon.svg'
+import shoppingCartIcon from '../img/shopping-cart-icon.svg'
+import spanishIcon from '../img/spanish-icon.svg'
+import registerIcon from '../img/register-icon.svg'
+import { render } from '@testing-library/react';
+import { User } from '../shared/shareddtypes';
 
 interface HeaderProps {
   setUser:(user:string) => void
@@ -39,45 +50,97 @@ const Header: FC<HeaderProps> = (props: HeaderProps) => {
   const logOut = () => {
     localStorage.removeItem("token");
     props.setUser("not logged");
+    console.log(localStorage.getItem("currentUser"))
   }
+  const [greet, setGreet] = useState("Hi," + user)
+
   if (localStorage.getItem("currentUser") !== "not logged") {
-    console.log(localStorage.getItem("currentUser")?.length);
       return (
-   <Navbar id="basic-navbar-nav">
-    {
-      <Nav>
+        <Nav>
         {
-          <Fragment>
-            <Button onClick={() => chooseLanguageHandler('EN')}>EN</Button>  
-            <Button onClick={() => chooseLanguageHandler('ES')}>ES</Button> 
-            <Nav.Item key = "home">
-              <Nav.Link onClick={logOut} href = "/">
-                {translate('navbar.logout')}
-              </Nav.Link>
-              <label>
-            {localStorage.getItem("currentUser")}
-            </label>
-            </Nav.Item>
-          </Fragment>
+          <Nav className="container-fluid">
+            {
+              <Fragment>
+                <Navbar.Brand>
+                  <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top"/>
+                  DeDesktop
+                </Navbar.Brand>
+                <Nav.Link href="/home" className="float-left">
+                  <img alt="" src={homeIcon} width="20" height="20" className="d-inline-block align-top" />
+                  {translate('nav.home')}
+                </Nav.Link>
+                <Nav.Link href="/catalogue">
+                  <img alt="" src={catalogIcon} width="20" height="20" className="d-inline-block align-top" />
+                  {translate('nav.catalogue')}
+                </Nav.Link>
+                <Nav.Link onClick={logOut} href = "/login">
+                  <img alt="" src={logoutIcon} width="20" height="20" className="d-inline-block align-top" />
+                  {translate('nav.logout')}
+                </Nav.Link>
+                <Nav.Link href="/carrito">
+                  <img alt="" src={shoppingCartIcon} width="20" height="20" className="d-inline-block align-top" />
+                  {translate('nav.shoppingcart')}
+                </Nav.Link>
+                <NavDropdown title={translate('nav.languaje')} id="idioma-dropdown" className="ms-auto">
+                  <Dropdown.Item as="button" onClick={() => chooseLanguageHandler('ES')}>
+                    <img alt="" src={spanishIcon} width="20" height="20" className="d-inline-block align-top" />
+                    Español
+                  </Dropdown.Item>
+                  <Dropdown.Item as="button" onClick={() => chooseLanguageHandler('EN')}>
+                    <img alt="" src={englishIcon} width="20" height="20" className="d-inline-block align-top" />
+                    English
+                  </Dropdown.Item>
+                </NavDropdown>
+              </Fragment>
+            }
+          </Nav>
         }
       </Nav>
-    }
-</Navbar>
   );
   } else{
     return (
       <Nav>
       {
-        <Fragment>
-          <Button onClick={() => chooseLanguageHandler('EN')}>EN</Button>  
-          <Button onClick={() => chooseLanguageHandler('ES')}>ES</Button> 
-          <Nav.Item >
-            <Nav.Link onClick={() => logOut} href = "/login">
-            {translate('navbar.login')}
-            {localStorage.getItem("token")}            
-            </Nav.Link>
-          </Nav.Item>
-        </Fragment>
+        <Nav className="container-fluid">
+          {
+            <Fragment>
+              <Navbar.Brand>
+                <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top"/>
+                DeDesktop
+              </Navbar.Brand>
+              <Nav.Link href="/home" className="float-left">
+                <img alt="" src={homeIcon} width="20" height="20" className="d-inline-block align-top" />
+                {translate('nav.home')}
+              </Nav.Link>
+              <Nav.Link href="/catalogue">
+                <img alt="" src={catalogIcon} width="20" height="20" className="d-inline-block align-top" />
+                {translate('nav.catalogue')}
+              </Nav.Link>
+              <Nav.Link href="/signup">
+                <img alt="" src={registerIcon} width="20" height="20" className="d-inline-block align-top" />
+                {translate('nav.register')}
+              </Nav.Link>
+              <Nav.Link onClick={logOut} href = "/login">
+                <img alt="" src={loginIcon} width="20" height="20" className="d-inline-block align-top" />
+                {translate('nav.login')}
+              </Nav.Link>
+              <Nav.Link href="/carrito">
+                <img alt="" src={shoppingCartIcon} width="20" height="20" className="d-inline-block align-top" />
+                {translate('nav.shoppingcart')}
+              </Nav.Link>
+              <NavDropdown title={translate('nav.languaje')} id="idioma-dropdown" className="ms-auto">
+                <Dropdown.Item as="button" onClick={() => chooseLanguageHandler('ES')}>
+                  <img alt="" src={spanishIcon} width="20" height="20" className="d-inline-block align-top" />
+                  Español
+                </Dropdown.Item>
+                <Dropdown.Item as="button" onClick={() => chooseLanguageHandler('EN')}>
+                  <img alt="" src={englishIcon} width="20" height="20" className="d-inline-block align-top" />
+                  English
+                </Dropdown.Item>
+              </NavDropdown>
+            </Fragment>
+          }
+        </Nav>
       }
     </Nav>
     
