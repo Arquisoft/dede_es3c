5ec5d56f581545197e774782ad2a61747dc40fdb
Sergio Arroni <sergioadgi38@gmail.com>
2022-03-31 20:16:16 +0200

    Prueba tests api.rest

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -15,32 +15,16 @@ let app:Application;
 let server:http.Server;
 
 beforeAll(async () => {
-    app = express();
-    const port: number = 5000;
-    const options: cors.CorsOptions = {
-        origin: ['http://localhost:3000']
-    };
-    const setDB = async (): Promise<boolean> => { 
-        const databaseName: string ='test';
-        if ( await database.setDB(databaseName) ) {
-            console.log(`Database connection established to ${databaseName}`);
-            app.set('db', database.getDB());
-            return true;
-        } else {
-            console.log(`Error on database connection to ${databaseName}`);
-            return false;
-        }
-    };    
-    app.use(cors(options));
-    app.use(bp.json());
-    await setDB();
-    app.use("/api", api)
-
-    server = app.listen(port, ():void => {
-        console.log('Restapi server for testing listening on '+ port);
-    }).on("error",(error:Error)=>{
-        console.error('Error occured: ' + error.message);
-    });
+    const databaseName: string = process.env.DATABASE_NAME || 'default';
+    if ( await database.setDB(databaseName) ) {
+        console.log(`Database connection established to ${databaseName}`);
+        await database.getDB().synchronize();
+        app.set('db', database.getDB());
+        return true;
+    } else {
+        console.log(`Error on database connection to ${databaseName}`);
+        return false;
+    }
 });
 
 afterAll(async () => {
