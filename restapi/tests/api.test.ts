e01f8486ded9cbffd72525f6df0f527882ca3752
Sergio Arroni <sergioadgi38@gmail.com>
2022-04-02 18:25:03 +0200

    Fix tests

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -1,8 +1,3 @@
-/**
- * Database Manager class
- * @author: Sergio Arroni del Riego - UO276341
- */
-import { createConnection, getConnectionOptions, Connection } from "typeorm";
 import request, {Response} from 'supertest';
 import express, { Application } from 'express';
 import * as http from 'http';
@@ -15,39 +10,48 @@ let app:Application;
 let server:http.Server;
 
 beforeAll(async () => {
-    
+    app = express();
+    const port: number = 5000;
+    const options: cors.CorsOptions = {
+        origin: ['http://localhost:3000']
+    };
+    const setDB = async (): Promise<boolean> => { 
+        const databaseName: string ='test';
+        if ( await database.setDB(databaseName) ) {
+            console.log(`Database connection established to ${databaseName}`);
+            app.set('db', database.getDB());
+            return true;
+        } else {
+            console.log(`Error on database connection to ${databaseName}`);
+            return false;
+        }
+    };    
+    app.use(cors(options));
+    app.use(bp.json());
+    await setDB();
+    app.use("/api", api)
+
+    server = app.listen(port, ():void => {
+        console.log('Restapi server for testing listening on '+ port);
+    }).on("error",(error:Error)=>{
+        console.error('Error occured: ' + error.message);
+    });
 });
 
 afterAll(async () => {
-    
+    server.close() //close the server
 })
 
-describe('user ', () => {
-    /**
-     * Tests that a user can be register
-     */
-     it('can be register correctly', async () => {
-        const user = {
-            username:"Dios",
-            email:"Dios@jaja.com",
-            password:"123456",
-            rol:"Admin"
-        }
-        const response:Response = await request(app).post('/api/register').send(user).set('Accept', 'application/json')
-        expect(response.statusCode).toBe(200);
-    });
 
+describe("products", () => {
     /**
-     * Tests that a user can be register with error
+     * Test that we can list products without any error.
      */
-     it('can be register correctly', async () => {
-        const user = {
-            username:"Dios",
-            email:"Dios@jaja.com",
-            rol:"Admin"
-        }
-        const response:Response = await request(app).post('/api/register').send(user).set('Accept', 'application/json')
-        expect(response.statusCode).toBe(500);
+    it("can be listed", async () => {
+      const response: Response = await request(app).get("/api/products");
+      console.log(response.text);
+      expect(response.statusCode).toBe(200);
     });
-
-});
+  
+  });
+  
