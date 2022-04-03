c6db2e4e0d5bdba7e08ee9c3e5ea20a4d7200313
SoniaFernandezCoto <79599350+SoniaFernandezCoto@users.noreply.github.com>
2022-04-03 15:28:53 +0200

    Test de pedidos

--- webapp/src/pages/OrdersPage.tsx
+++ webapp/src/pages/OrdersPage.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState, useEffect} from "react";
+import React, {FC, useState, useEffect, useContext} from "react";
 import Header from "../components/Header"
 import { Order } from "../shared/shareddtypes";
 import { getOrders, getOrdersByEmail, getUser } from "../api/api";
@@ -8,13 +8,15 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import { Paper, Table, TableContainer, TableHead } from "@mui/material";
+import { LangContext } from "../lang";
 
 interface OrdersPageProps {
-    translate: (key: string) => string
+    //translate: (key: string) => string
     setUser:(user:string) => void
 }
 
 const OrdersPage: FC<OrdersPageProps> = (props: OrdersPageProps) => {
+  const { dispatch: { translate } } = useContext(LangContext);
     const [orders, setOrders] = useState<Order[]>([]);
     const reloadItems = async () => {
       if (localStorage.getItem("currentUser") !== null && !localStorage.getItem("currentUser")?.includes("admin")){
@@ -27,21 +29,20 @@ const OrdersPage: FC<OrdersPageProps> = (props: OrdersPageProps) => {
       } else if (localStorage.getItem("currentUser") !== null && localStorage.getItem("currentUser")?.includes("admin")){
         setOrders(await getOrders());
       }
-      console.log(orders);
   }
 
   useEffect(() => {
     reloadItems();
 }, []);
-
     if (orders.length === 0){
         return (
         <div className="main">
-            <Header setUser={props.setUser}/>
-            <h1>{props.translate("orders.title")}</h1>
+         
+          {console.log(localStorage.getItem("currentUser"))}
+            <h1>{translate("orders.title")}</h1>
             <div className="mainEmptyContainer" style={{display: 'flex',justifyContent:'center', alignItems:'center', height: '100vh'}}>
-            <h2>{props.translate("orders.empty")}</h2>
-            <Button href="/catalog" >{props.translate("orders.shopping")}</Button>  
+            <h2>{translate("orders.empty")}</h2>
+            <Button href="/catalog" >{translate("orders.shopping")}</Button>  
             </div>
         </div>
         );
@@ -51,26 +52,26 @@ const OrdersPage: FC<OrdersPageProps> = (props: OrdersPageProps) => {
     return (
         <div className="mainContainer" style={{alignContent:"center", alignItems:"center", alignSelf:"center"}}>
             <Header setUser={props.setUser}/>
-            <h1>{props.translate("orders.title")}</h1>
+            <h1>{translate("orders.title")}</h1>
             <div style={{ alignContent:"center", alignItems:"center" }}>
               <TableContainer component={Paper} sx={{maxHeight: "440",maxWidth: "1000" , alignSelf:"center"}}>
               <Table stickyHeader aria-label="sticky table">
                 <TableHead>
                   <TableRow>
                   <TableCell align="center" colSpan={1}>
-                      {props.translate("orders.preview")}
+                      {translate("orders.preview")}
                     </TableCell>
                     <TableCell align="center" colSpan={2}>
-                      {props.translate("orders.id")}
+                      {translate("orders.id")}
                     </TableCell>
                     <TableCell align="center" colSpan={3}>
-                    {props.translate("orders.products")}
+                    {translate("orders.products")}
                     </TableCell>
                     <TableCell align="center" colSpan={4}>
-                    {props.translate("orders.price")}
+                    {translate("orders.price")}
                     </TableCell>
                     <TableCell align="center" colSpan={5}>
-                    {props.translate("orders.iva")}
+                    {translate("orders.iva")}
                     </TableCell>
                   </TableRow>
                 </TableHead>
@@ -81,7 +82,7 @@ const OrdersPage: FC<OrdersPageProps> = (props: OrdersPageProps) => {
             </TableContainer>
             </div>
             <div style={{alignContent:"center"}}>
-            <Button href="/catalog">{props.translate("orders.shopping")}</Button>
+            <Button href="/catalog">{translate("orders.shopping")}</Button>
             </div>
         </div>
         
---
+++ webapp/src/test/OrderPage.test.tsx
@@ -0,0 +1,65 @@
+import { render, screen } from "@testing-library/react";
+import HomePage from '../pages/HomePage';
+import LangState from "../lang";
+import UserState from "../lang";
+import React from "react";
+import OrdersPage from "../pages/OrdersPage";
+import DisplayOrders from "../components/DisplayOrders";
+import { Order, Product, ProductInOrder } from "../shared/shareddtypes";
+
+beforeAll(async () => {
+});
+
+afterAll(async () => {
+
+});
+
+test('check orders page text renders properly when the user has no orders', async () => {
+  localStorage.setItem("currentUser", "user");
+    render(
+        <React.StrictMode>
+        <UserState>
+        <LangState>
+          <OrdersPage setUser={() => "user"}/>
+        </LangState>
+        </UserState>
+      </React.StrictMode>,
+   )
+    const linkElement = screen.getByText(/My orders/i);
+    const noOrders = screen.getByText(/Go Shopping/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(noOrders).toBeInTheDocument();
+});
+
+test('check orders page text renders properly when the user has orders', async () => {
+  localStorage.setItem("currentUser", "Wardell Stephen Curry II");
+  const product: Product = {
+    amount: 2,
+    description: "test",
+    name: "product-test",
+    category: "test",
+    price: 20,
+    urlPhoto: "nan"
+  }
+  const productsOrder: ProductInOrder[] = [{id: "monitor", 
+                                            product: product,
+                                            quantity: 1}];
+  const orders: Order[] = [{ id: "123",
+                            price: 50,
+                            priceIVA: 70,
+                            products: productsOrder,
+                            user: "chef@gmail.com" }];
+    render(
+        <React.StrictMode>
+        <UserState>
+        <LangState>
+          <DisplayOrders orders={orders}/>
+        </LangState>
+        </UserState>
+      </React.StrictMode>,
+   )
+    const linkElement = screen.getByText(/123/i);
+    const noOrders = screen.getByText(/product-test/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(noOrders).toBeInTheDocument();
+});
