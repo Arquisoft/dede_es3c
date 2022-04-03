e5b69a5ae7428e99393293d2444f347c31b81e16
SoniaFernandezCoto <79599350+SoniaFernandezCoto@users.noreply.github.com>
2022-04-03 02:32:31 +0200

    Arreglado test homepage + cambios en algunas props

---
+++ webapp/src/test/HomePage.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import HomePage from '../pages/HomePage';
+import LangState from "../lang";
+import UserState from "../lang";
+import React from "react";
+
+beforeAll(async () => {
+});
+
+afterAll(async () => {
+
+});
+
+test('check home page text renders properly (english)', async () => {
+    render(
+        <React.StrictMode>
+        <UserState>
+        <LangState>
+          <HomePage setUser={() => "user"}/>
+        </LangState>
+        </UserState>
+      </React.StrictMode>,
+   )
+    const linkElement = screen.getByText(/DeDesktop is the result of the effort and dedication of the es3c group of the Software Architecture subject./i);
+    const linkButton = screen.getByText(/See all our products in our catalog/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkButton).toBeInTheDocument();
+});
