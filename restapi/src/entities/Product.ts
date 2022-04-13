63171658f23c7839a52a5ffb413f72ba7e06330b
Sergio Arroni <sergioadgi38@gmail.com>
2022-04-13 23:20:54 +0200

    Fix code smells

--- restapi/src/entities/Product.ts
+++ restapi/src/entities/Product.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, ObjectIdColumn, PrimaryColumn, Unique } from "typeorm";
+import { Entity, Column, ObjectIdColumn, PrimaryColumn } from "typeorm";
 import {v4 as uuidv4} from 'uuid';
 
 @Entity()
