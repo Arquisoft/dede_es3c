94eb3805bf63d576debaf2eac440ab18f413d28e
Sergio Arroni <sergioadgi38@gmail.com>
2022-04-25 21:31:52 +0200

    Inicializar tests e2e

--- webapp/e2e/steps/register-form.steps.ts
+++ webapp/e2e/steps/register-form.steps.ts
@@ -23,26 +23,6 @@ defineFeature(feature, test => {
 
   test('The user is not registered in the site', ({given,when,then}) => {
     
-    let email:string;
-    let username:string;
-
-    given('An unregistered user', () => {
-      email = "newuser@test.com"
-      username = "newuser"
-    });
-
-    when('I fill the data in the form and press submit', async () => {
-      await expect(page).toMatch('Hi, ASW students')
-      await expect(page).toFillForm('form[name="register"]', {
-        username: username,
-        email: email,
-      })
-      await expect(page).toClick('button', { text: 'Accept' })
-    });
-
-    then('A confirmation message should be shown in the screen', async () => {
-      await expect(page).toMatch('You have been registered in the system!')
-    });
   })
 
   afterAll(async ()=>{
