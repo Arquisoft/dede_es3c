e4a773cfc75e51bb2f288c96ab590833c2734a0d
Sergio Arroni <sergioadgi38@gmail.com>
2022-05-03 22:55:57 +0200

    TODO

--- webapp/e2e/steps/flow.steps.ts
+++ webapp/e2e/steps/flow.steps.ts
@@ -11,8 +11,7 @@ defineFeature(feature, test => {
     beforeAll(async () => {
         browser = process.env.GITHUB_ACTIONS
             ? await puppeteer.launch()
-            //: await puppeteer.launch({ headless: true });
-            : await puppeteer.launch({ headless: false, slowMo: 0 });
+            : await puppeteer.launch({ headless: true });
         page = await browser.newPage();
 
         await page
